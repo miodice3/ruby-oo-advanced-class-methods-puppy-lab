304e4de6b4d4997e63125c978732c83c27fce289
Michael Iodice <miodice3@gmail.com>
2020-10-13 19:45:18 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,49 @@
-# Add your code here
+require 'pry'
+class Dog
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        #@@all << name
+        self.save
+        #@@all << self
+        #puts @@all
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each do |name|
+            puts name.name
+        end
+    end
+
+end
+
+# test = Dog.new("Bently")
+# test2 = Dog.new("BentlyJr")
+# # #binding.pry
+#  #puts test.name
+
+# # puts "break before print all"
+
+# Dog.print_all
